b20d291a7dae8508494ae3329366ef499ddb72b5
martinhj <martinhj@gmail.com>
2013-04-25 18:39:43 +0200

    Refacturated the code. Doing the same.

--- Main.java
+++ Main.java
@@ -1,15 +1,25 @@
 class Main {
+	final int DEVIDE_BY = 16;
+	// Array to be sorted.
+	int tall [] = new int [Integer.MAX_VALUE / DEVIDE_BY];
+	long time;
+	public Main() {
+		initialSetup();
+	}
 	public static void main (String [] args) {
-		final int DEVIDE_BY = 16;
-		int tall [] = new int [Integer.MAX_VALUE / DEVIDE_BY];
+		new Main();
+	}
+	void initialSetup() {
+		// Fill array with all sorts of numbers.
+		System.out.print("Number of places in the array to be sorted: ");
 		System.out.println(Integer.MAX_VALUE / DEVIDE_BY);
-		System.out.println(System.currentTimeMillis());
-		for (int i = 0; i < Integer.MAX_VALUE / DEVIDE_BY; i++)
-			tall[i] = i;
-		System.out.println(System.currentTimeMillis());
+		System.out.println("The current time in milliseconds before we start"
+			+ " to fill the array: ");
+		System.out.println(time = System.currentTimeMillis());
 		for (int i = 0; i < Integer.MAX_VALUE / DEVIDE_BY; i++)
 			tall[i] = (int) (Math.random() * Integer.MAX_VALUE);
-
-		System.out.println(System.currentTimeMillis());
+		System.out.println("Milliseconds after we have filled the array");
+		System.out.println(System.currentTimeMillis() - time);
+		System.out.println("Finished filling the array, let's move on.");
 	}
 }
