3b95f97634d2860c65c69f5ff33ad1708b0ba0cb
martinhj <martinhj@gmail.com>
2013-04-25 15:39:27 +0200

    Testet hvor lang tid det tar å lage en array 134 millioner plasser og fylle den med en lineær loop. => Litt over 1,6 tiendedel.

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 class Main {
 	public static void main (String [] args) {
+		final int DEVIDE_BY = 16;
+		int tall [] = new int [Integer.MAX_VALUE / DEVIDE_BY];
+		System.out.println(Integer.MAX_VALUE / DEVIDE_BY);
 		System.out.println(System.currentTimeMillis());
+		for (int i = 0; i < Integer.MAX_VALUE / DEVIDE_BY; i++)
+			tall[i] = i;
+
 		System.out.println(System.currentTimeMillis());
 	}
 }
