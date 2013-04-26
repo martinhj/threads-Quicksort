029726bd730b97c1cf09f9a3e9468044ab2b9659
martinhj <martinhj@gmail.com>
2013-04-26 13:06:52 +0200

    Fixed linear least and added linear last

--- FindLeast.java
+++ FindLeast.java
@@ -5,10 +5,14 @@ class FindLeast {
 		//this.array = array;
 		findLeast(array);//(this.array);
 		int n = array[0];
+		int m = array[0];
 		// Linear least
-		for (int i = 1; i < array.length; i++)
-			if (n < array[i]) n = array[i];
+		for (int i = 1; i < array.length; i++) {
+			if (n > array[i]) n = array[i];
+			if (m < array[i]) m = array[i];
+		}
 		System.out.println("Linear least: " + n);
+		System.out.println("Linear last: " + m);
 
 	}
 	private int findLeast(int [] array) {
