79e0a0dc316ce3df06408b3232ec874661c46f64
martinhj <martinhj@gmail.com>
2013-04-26 10:35:34 +0200

    Decided to make it generic.

--- Traverser.java
+++ Traverser.java
@@ -1,4 +1,5 @@
-class Traverser implements Runnable{
+import java.lang.Comparable;
+class Traverser<K extends Comparable> implements Runnable{
 	/** 
 	 * Constructor for a runnable that will devide the array until the number
 	 * of objects in the array is below 100. If it is, find the least of these
@@ -7,8 +8,12 @@ class Traverser implements Runnable{
 	Traverser() {
 
 	}
-	public void run() {
-
+	/**
+	 * Method that devides an array in two and calls itself with the two new 
+	 * arrays as arguments and continues this until the length of the array is
+	 * below 100. Then it will find the least of these and return it.
+	 */
+	public K run() {
+		return null;
 	}
-
 }
