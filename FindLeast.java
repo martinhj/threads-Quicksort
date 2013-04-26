e162d7e4ea508dd32c2894a89b29b23ab7a99aa3
martinhj <martinhj@gmail.com>
2013-04-26 12:44:41 +0200

    Added all the printouts from the generics branch

--- FindLeast.java
+++ FindLeast.java
@@ -22,14 +22,19 @@ class FindLeast {
 			// the length of this array is set the old length - the new length
 			// to get all numbers if the length of the old array were uneven.
 			arrayPart2 = fillArray(array, array.length - newLength, newLength);
+			System.out.println("First array: " + arrayPart1.length);
+			System.out.println("Second array: " + arrayPart2.length);
 
 		}
 		return -1;
 	}
 	private int [] fillArray(int [] oldArray, int length, int start) {
+		System.out.println("The old array's size: " + oldArray.length);
+		System.out.println("Point to start: " + start);
 		int [] newArray = new int[length];
 		for (int i = start; i < length; i++)
 			newArray[i] = oldArray[start + i];
+		System.out.println("The new array's size: " + newArray.length);
 		return newArray;
 
 	}
