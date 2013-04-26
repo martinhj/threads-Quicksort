a0294856dabbe820c2b13f1c7da0c0a6ba6e1017
martinhj <martinhj@gmail.com>
2013-04-26 10:27:59 +0200

    Added Traverser that will contain the methods to be run in the threads

---
+++ Traverser.java
@@ -0,0 +1,14 @@
+class Traverser implements Runnable{
+	/** 
+	 * Constructor for a runnable that will devide the array until the number
+	 * of objects in the array is below 100. If it is, find the least of these
+	 * numbers and return it.
+	 */
+	Traverser() {
+
+	}
+	public void run() {
+
+	}
+
+}
